d7f231c34b097b7315c09b5827d65a48b7614eed
MinhNguyen153216 <minhnguyen153216@gmail.com>
2022-01-05 12:15:41 +0700

    add resources file

---
+++ karaokemanager/src/main/java/web/project/karaokemanager/KaraokemanagerApplication.java
@@ -0,0 +1,15 @@
+package web.project.karaokemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaraokemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaraokemanagerApplication.class, args);
+	}
+
+}
+
+
---
+++ karaokemanager/src/main/java/web/project/karaokemanager/controller/Controller.java
@@ -0,0 +1,5 @@
+package web.project.karaokemanager.controller;
+
+public class Controller {
+    
+}
